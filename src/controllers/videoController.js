80656bf12317cbf5bb44d707fe0640e1746dedd1
이송현 <isonghyeon@isonghyeon-ui-MacBookPro.local>
2022-07-31 16:15:10 +0900

    :heavy_plus_sign: Add - Planning Routers

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,3 +1,7 @@
 export const trending = (req, res) => res.send("Home Page Videos");
 export const watch = (req, res) => res.send("Watch");
 export const edit = (req, res) => res.send("Edit");
+export const search = (req, res) => res.send("Search");
+export const see = (req, res) => res.send("See Video");
+export const deleteVideo = (req, res) => res.send("Delete Video");
+export const upload = (req, res) => res.send("Upload Video");
