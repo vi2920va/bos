0e134a1792c75d1c0530e304de9acd6d69787879
이송현 <isonghyeon@isonghyeon-ui-MacBookPro.local>
2022-08-01 08:01:16 +0900

    :heavy_plus_sign: Add - URL Parametrs

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,7 +1,14 @@
 export const trending = (req, res) => res.send("Home Page Videos");
-export const watch = (req, res) => res.send("Watch");
-export const edit = (req, res) => res.send("Edit");
+export const edit = (req, res) => {
+  console.log(`📺 Edit ${req.params}`);
+  return res.send("Edit");
+};
 export const search = (req, res) => res.send("Search");
-export const see = (req, res) => res.send("See Video");
-export const deleteVideo = (req, res) => res.send("Delete Video");
+export const see = (req, res) => {
+  console.log(`📺 See ${req.prams}`);
+  return res.send("See Video");
+};
+export const deleteVideo = (req, res) => {
+  return res.send("Delete Video");
+};
 export const upload = (req, res) => res.send("Upload Video");
