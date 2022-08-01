69ef693427de1984f67f82c38db1695c18323a16
이송현 <isonghyeon@isonghyeon-ui-MacBookPro.local>
2022-08-01 09:24:42 +0900

    :heavy_plus_sign: Add - Returning HTML

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,10 +1,20 @@
-export const trending = (req, res) => res.send("Home Page Videos");
-export const edit = (req, res) => {
-  return res.send("Edit");
+export const trending = (req, res) => {
+  res.send(
+    "<!DOCTYPE html><html lang='ko'><head><title>Wetube</title></head><body><h1>Home</h1><footer>&copy;2022 Wetube -  All rights reserved</footer></body></html>"
+  );
 };
+
+export const edit = (req, res) => {
+  return res.send(
+    `<!DOCTYPE html><html lang='ko'><head><title>Wetube</title></head><body><h1>Edit video #${req.params.id}</h1><footer>&copy;2022 Wetube -  All rights reserved</footer></body></html>`
+  );
+};
+
 export const search = (req, res) => res.send("Search");
 export const see = (req, res) => {
-  return res.send("See Video");
+  return res.send(
+    `<!DOCTYPE html><html lang='ko'><head><title>Wetube</title></head><body><h1>Watch video #${req.params.id}</h1><footer>&copy;2022 Wetube -  All rights reserved</footer></body></html>`
+  );
 };
 export const deleteVideo = (req, res) => {
   return res.send("Delete Video");
